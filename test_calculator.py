424bba4f0458cee58eb20f6c7689fd2d5041d20f
Kristina-Despotuli <kristina.despotuli@gmail.com>
2026-04-08 09:05:49 -0400

    modified test_calculator

--- test_calculator.py
+++ test_calculator.py
@@ -11,11 +11,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(0,3), 0)
+        self.assertEqual(mul(4, 3), 12)
+        self.assertEqual(mul(-5, 8), -40)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(1,4),4)
+        self.assertEqual(div(2, 4), 2)
+        self.assertEqual(div(7, 2), 3.5)
     # ##########################
 
     ######## Partner 2
@@ -34,21 +38,21 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            logarithmic(0, 10)
+
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3,4), 5)
+        self.assertEqual(hypotenuse(5, 12), 13)
+        self.assertAlmostEqual(hypotenuse(1.5, 2.0), (1.5**2 + 2.0**2) ** 0.5)
     #     fill in code
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
-
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self): # 3 assertions
+        with self.assertRaises(ValueError):
+            square_root(-5)
+        self.asserEqual(square_root(9), 3)
+        self.asserAlmostEqual(square_root(2), 2 ** 0.5)
     ##########################
 
 # Do not touch this
