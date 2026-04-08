7cd3f677cf6c7008b4c6a091e83ef1fd59157347
Kristina-Despotuli <kristina.despotuli@gmail.com>
2026-04-08 08:52:39 -0400

    modified calculator

--- calculator.py
+++ calculator.py
@@ -14,31 +14,20 @@ def hypotenuse(a, b):
     return math.hypot(a, b)
 def add(a, b): 
     return a + b
-def sub(a, b):
-    return a - b
 def mul(a, b):
     return a * b
 def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b/a
-def log(a, b):
-    if a <= 0 or b <=0:
-        raise ValueError
-    return math.log(b, a)
 def exp(a, b):
     return a ** b
 def subtract(a,b):
     return a - b
-def multiply(a, b):
-    return a * b
-
 def logarithmic(a,b):
     if b <= 0 or a <= 0:
         raise ValueError
     return math.log(b, a)
-def exponent(a, b):
-    a**b
 
 
 
